f7f2c8356d1dbee62260ba0420ec84fe2ee972d8
Marcos Henrique <makikin18@hotmail.com>
2021-03-24 17:46:50 -0300

    feat(create login page and run tests): add login page and using cypress to run tests
    cypress test

---
+++ pages/app/profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ProfilePage() {
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div>
+      Página de Profile!
+      <img
+        src="https://media.giphy.com/media/bn0zlGb4LOyo8/giphy.gif"
+        alt="Nicolas Cage"
+      />
+    </div>
+  );
+}
